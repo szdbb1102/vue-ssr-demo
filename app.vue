e71d1bdc3dad2a8f57c095313a31c89898fa8a5b
xuexin <xuexin@cloudwalk.cn>
2020-06-02 17:08:17 +0800

    nothing

---
+++ app.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    app
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+
+    }
+  },
+  components: {
+
+  },
+  methods: {
+
+  },
+}
+</script>
+
+<style scoped>
+</style>
