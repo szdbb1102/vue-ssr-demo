3eed11f9d0e5884f64205ddd474b00a7cac86e2f
xuexin <xuexin@cloudwalk.cn>
2020-08-04 14:11:05 +0800

    feat: 封装模板

--- guide.md
+++ guide.md
@@ -1,9 +1,5 @@
-## 初始化
-1. 安装依赖
-   1. vue vue-server-renderer
-   2. express
-2. 最暴露本质的代码 [demo](./server.js)
-   1. 本质 把vue组件编译为html
-      1. 后面再激活为vue组件
-   2. const renderer = require('vue-server-renderer').createRenderer()
-   3. renderer.renderToString(app, (err, html) => {})
+## 使用模板
+1. 目的 想把之前写死的html模板抽出来
+2. 做法
+   1. 建立单独文件 fs读取
+   2. 配合ssr的模板api
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const Vue = require('vue')
 const server = require('express')()
-const renderer = require('vue-server-renderer').createRenderer()
+const renderer = require('vue-server-renderer').createRenderer({
+  template: require('fs').readFileSync('./index.template.html', 'utf-8')
+})
 
 server.get('*', (req, res) => {
   const app = new Vue({
@@ -15,13 +17,7 @@ server.get('*', (req, res) => {
       res.status(500).end('Internal Server Error')
       return
     }
-    res.end(`
-      <!DOCTYPE html>
-      <html lang="en">
-        <head><title>Hello</title><meta charset="UTF-8"></head>
-        <body>${html}</body>
-      </html>
-    `)
+    res.end(`${html}`)
   })
 })
 
